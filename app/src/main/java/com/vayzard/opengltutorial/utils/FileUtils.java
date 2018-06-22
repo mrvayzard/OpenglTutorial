71b33960b2d5a86101b241af442cc1d79be7e961
Denys Yablonskyi <mrvayzard@ukr.net>
2018-06-22 18:18:38 +0300

    Add method for read text file from raw resources

---
+++ app/src/main/java/com/vayzard/opengltutorial/utils/FileUtils.java
@@ -0,0 +1,33 @@
+package com.vayzard.opengltutorial.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.RawRes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FileUtils {
+
+    public static String readTextFileFromResource(Context context, @RawRes int resourceId) {
+        StringBuilder body = new StringBuilder();
+        try {
+            InputStream inputStream =
+                    context.getResources().openRawResource(resourceId);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String nextLine;
+            while ((nextLine = bufferedReader.readLine()) != null) {
+                body.append(nextLine);
+                body.append('\n');
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Could not open resource: " + resourceId, e);
+        } catch (Resources.NotFoundException nfe) {
+            throw new RuntimeException("Resource not found: " + resourceId, nfe);
+        }
+        return body.toString();
+    }
+}
