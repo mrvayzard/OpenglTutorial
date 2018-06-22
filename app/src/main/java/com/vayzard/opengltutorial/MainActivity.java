2bd2ca8f4669e7f84ac5e016078a18831f646883
Denys Yablonskyi <mrvayzard@ukr.net>
2018-06-22 15:16:50 +0300

    Add red screen

--- app/src/main/java/com/vayzard/opengltutorial/MainActivity.java
+++ app/src/main/java/com/vayzard/opengltutorial/MainActivity.java
@@ -1,13 +1,65 @@
 package com.vayzard.opengltutorial;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
+
+    private void initOpengl(){
+        final ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo =
+                activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 =
+                configurationInfo.reqGlEsVersion >= 0x20000
+                        || Build.FINGERPRINT.startsWith("generic")
+                        || Build.FINGERPRINT.startsWith("unknown")
+                        || Build.MODEL.contains("google_sdk")
+                        || Build.MODEL.contains("Emulator")
+                        || Build.MODEL.contains("Android SDK built for x86");
+
+        if (supportsEs2) {
+            // Request an OpenGL ES 2.0 compatible context.
+            glSurfaceView.setEGLContextClientVersion(2);
+            // Assign our renderer.
+            glSurfaceView.setRenderer(new SimpleRenderer());
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        glSurfaceView = new GLSurfaceView(this);
+        setContentView(glSurfaceView);
+        initOpengl();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(rendererSet){
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(rendererSet){
+            glSurfaceView.onResume();
+        }
     }
 }
---
+++ app/src/main/java/com/vayzard/opengltutorial/SimpleRenderer.java
@@ -0,0 +1,31 @@
+package com.vayzard.opengltutorial;
+
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
+public class SimpleRenderer implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+}
