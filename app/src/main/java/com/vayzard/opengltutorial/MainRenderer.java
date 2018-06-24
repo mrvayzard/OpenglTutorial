632e697cebb1dabf501b94d31366be307adecd1d
Vayzard <mrvayzard@ukr.net>
2018-06-24 03:23:05 -0700

    Add fake brightness for table

--- app/src/main/java/com/vayzard/opengltutorial/MainRenderer.java
+++ app/src/main/java/com/vayzard/opengltutorial/MainRenderer.java
@@ -17,52 +17,51 @@ import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.GL_LINES;
 import static android.opengl.GLES20.GL_POINTS;
-import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
 import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
-import static android.opengl.GLES20.glGetUniformLocation;
-import static android.opengl.GLES20.glUniform4f;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
 
 public class MainRenderer implements GLSurfaceView.Renderer {
 
-    private static final String U_COLOR = "u_Color";
-    private int uColorLocation;
     private static final String A_POSITION = "a_Position";
-    private int aPositionLocation;
-
     private static final int POSITION_COMPONENT_COUNT = 2;
     private static final int BYTES_PER_FLOAT = 4;
+
+    private static final String A_COLOR = "a_Color";
+    private static final int COLOR_COMPONENT_COUNT = 3;
+    private static final int STRIDE =
+            (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+
+    private int aPositionLocation;
+    private int aColorLocation;
     private final FloatBuffer vertexData;
+
     private Context context;
 
     public MainRenderer(Context context) {
         this.context = context;
 
         float[] tableVerticesWithTriangles = {
-                // Triangle 1
-                -0.5f, -0.5f,
-                0.5f, 0.5f,
-                -0.5f, 0.5f,
-
-                // Triangle 2
-                -0.5f, -0.5f,
-                0.5f, -0.5f,
-                0.5f, 0.5f,
-
-
-                // Line 1
-                -0.5f, 0f,
-                0.5f, 0f,
-
-                // Mallets
-                0f, -0.25f,
-                0f, 0.25f
+            // Order of coordinates: X, Y, R, G, B
+            // Triangle Fan
+            0f, 0f, 1f, 1f, 1f,
+            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+            // Line 1
+            -0.5f, 0f, 1f, 0f, 0f,
+            0.5f, 0f, 1f, 0f, 0f,
+            // Mallets
+            0f, -0.25f, 0f, 0f, 1f,
+            0f, 0.25f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer
@@ -76,8 +75,10 @@ public class MainRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        String vertexShaderSource = FileUtils.readTextFileFromResource(context, R.raw.simple_vertex_shader);
-        String fragmentShaderSource = FileUtils.readTextFileFromResource(context, R.raw.simple_fragment_shader);
+        String vertexShaderSource = FileUtils.readTextFileFromResource(context,
+                R.raw.simple_vertex_shader);
+        String fragmentShaderSource = FileUtils.readTextFileFromResource(context,
+                R.raw.simple_fragment_shader);
         int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
         int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
         int program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
@@ -86,12 +87,18 @@ public class MainRenderer implements GLSurfaceView.Renderer {
             return;
 
         glUseProgram(program);
-        uColorLocation = glGetUniformLocation(program, U_COLOR);
+        aColorLocation = glGetAttribLocation(program, A_COLOR);
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
 
         vertexData.position(0);
-        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, 0, vertexData);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT,
+                false, STRIDE, vertexData);
         glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT,
+                false, STRIDE, vertexData);
+        glEnableVertexAttribArray(aColorLocation);
 
     }
 
@@ -104,15 +111,11 @@ public class MainRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl) {
         glClear(GL_COLOR_BUFFER_BIT);
         //draw triangles for table
-        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
         //draw divide line
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
         glDrawArrays(GL_LINES, 6, 2);
         //draw start points
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f); //red
         glDrawArrays(GL_POINTS, 8, 1);
-        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f); //blue
         glDrawArrays(GL_POINTS, 9, 1);
     }
 
